a0d9e78abc9554e8dbc05a18f79ed848a85bc4a5
giginaranjo <gilarynaranjop@gmail.com>
2024-08-11 19:50:35 -0300

    Creacion de clase productManager

--- src/app.js
+++ src/app.js
@@ -8,6 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({extende:true}));
 
 const server=app.listen(PORT,()=>{
-    console.log(`Server en puerto ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
     
 });
--- src/dao/productManager.js
+++ src/dao/productManager.js
@@ -1,3 +1,67 @@
-class ProductManager{
-    
+import fs from "fs";
+
+export class ProductManager {
+
+    static path
+
+    static async getProducts() {
+        if (fs.existsSync(this.path)) {
+            let products = JSON.parse(await fs.promises.readFile(this.path, { encoding: "utf-8" }))
+            return products
+        }else{
+            return []
+        }
+    }
+
+    static async addProduct(){
+        let products = await this.getProducts()
+
+        let id = (products.length > 0) ? this.products[this.products.length-1]+1 : 1;
+        
+        let newProduct = {
+            id,
+            title,
+            description,
+            code,
+            price,
+            status,
+            stock,
+            category,
+            thumbnail
+        }
+        products.push(newProduct)
+
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 5))
+        return newProduct
+    }
+
+    static async modifyProduct(id, modification={}){
+        let products = await this.getProducts()
+        let indexProduct = products.findIndex(p => p.id === id)
+        if(indexProduct ===-1){
+            throw new Error(`Id ${id} not found`)
+        }
+
+        products[indexProduct]={
+            ...products[indexProduct],
+            ...modification,
+            id
+        }
+
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 5))
+        return products[indexProduct]
+    }
+
+    static async deleteProduct(id){
+        let productsBefore = await this.getProducts()
+        let productsAfter = productsBefore.filter(p => p.id != id)
+
+        if(productsBefore.length === productsAfter){
+            throw new Error(`Product not found`)
+        }
+
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 5))
+        return productsAfter
+
+    }
 }
