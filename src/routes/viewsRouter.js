bd2e68fde49c5fd4733420f3f3d2a53be388c515
giginaranjo <gilarynaranjop@gmail.com>
2024-08-25 19:39:27 -0300

    home with product list

--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -1,11 +1,25 @@
 import { Router } from "express";
+import ProductsManager from "../dao/productManager.js"
 
 const router = Router()
 
-router.get("/", (req, res) => {
-    res.setHeader('Content-Type', 'text/html'); 
-    res.status(200).render('home')
+// Obtener listado de productos
+
+router.get("/", async (req, res) => {
+    let products
+    try {
+        products = await ProductsManager.getProducts()
+        res.setHeader('Content-Type', 'text/html'); 
+        res.status(200).render('home', {products})
+    } catch (error) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(500).json({
+            error: `Error inesperado en el servidor. Intente más tarde.`,
+            detalle: `${error.message}`
+        });
+    }
 })
+
 
 router.get("/realTimeProducts", (req, res) => {
 
