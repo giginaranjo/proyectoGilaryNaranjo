79e6c824a7929271c525a68e089614215efe051f
giginaranjo <gilarynaranjop@gmail.com>
2024-08-26 23:24:52 -0300

    dirname

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import __dirname from "./utils.js";
 import path from "path";
 import express from "express";
 import { Server } from "socket.io";
@@ -14,12 +15,12 @@ const app = express();
 
 app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
-app.set("views", path.join(import.meta.dirname, "/views"))
+app.set("views", path.join(__dirname, "/views"))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(import.meta.dirname, "/public")))
+app.use(express.static(path.join(__dirname, "/public")))
 
 app.use("/api/products", productsRouter)
 app.use("/api/carts", cartsRouter)
